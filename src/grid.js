d0fcd5ea7164b22a9fbac0537932b66572d43e61
justin roberts <Justin-Roberts2@live.nmit.ac.nz>
2024-04-04 14:57:09 +1300

    added a grid that needs improving

--- src/grid.js
+++ src/grid.js
@@ -1,1 +1,13 @@
 //Create a 2-D grid in an array
+const readlineSync = require('readline-sync');
+//error cannot find module readline error
+    let grid = [[" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "],]
+
+                console.log(grid)
+//grid needs improving
