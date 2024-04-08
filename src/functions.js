16009e9bb87dce7c8a8be0b424e0dbd2a8007216
justin roberts <Justin-Roberts2@live.nmit.ac.nz>
2024-04-09 11:12:36 +1200

    added some function comments and grid

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,18 @@
 //Post all functions in the file
+//use grid to call back to grid
+//0 is used a empty space in grid
+//1 is used as a user input
+//2 is used as a reproduct
+
+//Create a way to accept user input
+
+//Create a function where each cell with one or no neighbors dies,
+//as if by solitude.
+
+//Create a function where each cell with four or more neighbors dies,
+//as if by overpopulation.
+
+//Create a function where each cell with two or three neighbors survives.
+
+//Create a function where (For a space that is empty or unpopulated)
+//each cell with three neighbors becomes populated.
